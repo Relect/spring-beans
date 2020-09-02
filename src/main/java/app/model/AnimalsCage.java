a35139622d16a1a0739a1a2d2869dee755f3d966
user <Relect@bk.ru>
2020-09-02 16:51:55 +0300

    commit

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -8,14 +8,17 @@ import org.springframework.stereotype.Component;
 public class AnimalsCage {
 
     @Autowired
-    @Qualifier("Dog")
+    @Qualifier("dog")
     private Animal animal;
+
+    @Autowired
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer().getTime());
         System.out.println("________________________");
     }
 }
